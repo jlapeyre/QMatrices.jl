19d8e29d01c5e33fadb8afb65cf1e57096b95e85
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-05-11 14:19:36 -0700

    Files generated by PkgTemplates

---
+++ src/QMatrices.jl
@@ -0,0 +1,5 @@
+module QMatrices
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using QMatrices
+using Test
+
+@testset "QMatrices.jl" begin
+    # Write your own tests here.
+end
