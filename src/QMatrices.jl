36e95727c1798047c0c5b6271a4f4d3212c54feb
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-05-31 17:32:08 -0400

    Add docstring for QMatrices, bump version

--- src/QMatrices.jl
+++ src/QMatrices.jl
@@ -2,7 +2,31 @@
     module QMatrices
 
 Plain-data matrices and vectors for quantum information.
-`z0, z1, I2, X, Y, Z, H, S, T, sqrt_NOT, CX, CCX, CZ, SWAP`
+
+```
+z0, z1, plus, minus, iplus, iminus,
+I2, H, CH, CCH, S, T,
+sqrt_NOT,
+X, CX, CCX, Y, CY, CCY, Z, CZ, CCZ,
+SWAP, CSWAP, iSWAP, ECR,
+Rphi, Rphipi,
+SX,
+RX, RXpi,
+RY, RYpi,
+RZ, RZpi,
+RXX, RXXpi,
+RXZ, RXZpi,
+RZX, RZXpi,
+U, Ualt, U2,
+R, Rpi,
+magic_basis,
+random_unitary,
+random_unitary!,
+random_special_unitary,
+random_unitary_hermitian,
+random_normal,
+random_orthogonal
+```
 """
 module QMatrices
 
@@ -16,6 +40,7 @@ export I2, H, CH, CCH, S, T,
     X, CX, CCX, Y, CY, CCY, Z, CZ, CCZ,
     SWAP, CSWAP, iSWAP, ECR,
     Rphi, Rphipi,
+    SX,
     RX, RXpi,
     RY, RYpi,
     RZ, RZpi,
@@ -31,8 +56,6 @@ export I2, H, CH, CCH, S, T,
     random_unitary_hermitian,
     random_normal,
     random_orthogonal
-
-export SX
 
 include("basic_dynamic.jl")
 include("linalg.jl")
